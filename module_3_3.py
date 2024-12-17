1fcb6532f157e647592fdb29503d0d844b563f9f
LupineKIn <LazyTrainer@yandex.ru>
2024-12-18 01:44:29 +0300

    module_3_3.py

---
+++ module_3_3.py
@@ -0,0 +1,9 @@
+﻿def print_params(a = 1, b = 'строка', c = True):
+    print (a, b, c)
+print_params()
+values_list = [2, 'bbb', False]
+print_params(*values_list)
+values_dict = {'a':1.1 , 'b': 'строка', 'c': False}
+print_params(**values_dict)
+values_list_2 = ['ff', 1234]
+print_params(*values_list_2, 45)

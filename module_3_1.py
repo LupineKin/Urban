a5d3c49ab714c8d6b1491b3744a89471bfd95789
LupineKIn <LazyTrainer@yandex.ru>
2024-12-18 00:32:53 +0300

    add module_3_1.py

---
+++ module_3_1.py
@@ -0,0 +1,28 @@
+﻿import re
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    a = [len(string), string.upper(), string.lower()]
+    a = tuple(a)
+    return (a)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    a = False
+    list_to_search = [b.lower() for b in list_to_search]
+    if string.lower() in list_to_search:
+        a = True
+    return (a)
+
+
+calls = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)

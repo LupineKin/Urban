ffcfea196f65828bdcc4734dfa3206e6ca53cd71
LupineKIn <LazyTrainer@yandex.ru>
2024-12-19 05:28:41 +0300

    module_3_6.py

---
+++ module_3_6.py
@@ -0,0 +1,28 @@
+﻿def calculate_structure_sum(a):
+    summ = 0
+    if isinstance(a, (list, tuple, set)):
+        for i in a:
+            summ += calculate_structure_sum(i)
+    elif isinstance(a, dict):
+        for value in a.values():
+            summ += calculate_structure_sum(value)
+        for i in a:
+            summ += calculate_structure_sum(i)
+    else:
+        if isinstance (a, str):
+            summ += len(a)
+        else:
+            summ += a
+        
+    return summ
+
+
+data_structure = [
+    [1, 2, 3],
+    {"a": 4, "b": 5},
+    (6, {"cube": 7, "drum": 8}),
+    "Hello",
+    ((), [{(2, "Urban", ("Urban2", 35))}]),
+]
+
+print("Сумма: ", calculate_structure_sum(data_structure))
